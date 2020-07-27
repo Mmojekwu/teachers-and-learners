5d118fb6b0bdf000574338ea886d99663c325afa
Mmojekwu <lovelyjhames@gmail.com>
2020-07-27 15:05:33 +0100

    new upload

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\CYRIL\Documents\DEV\LMS-working\teach-me-django\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
